5adf6a72918286fdf4a398b81c29dcfcdad28c95
Julio Eduardo B. Livelo <94818606+yongLivelo@users.noreply.github.com>
2025-01-04 01:02:13 +0800

    final

--- src/components/layout/CalledBalls/components/History.tsx
+++ src/components/layout/CalledBalls/components/History.tsx
@@ -5,10 +5,10 @@ export default function History() {
     <>
       <div className="flex justify-around gap-4 rounded bg-slate-900 p-4">
         <div>
-          <div>Number of Calsds</div>
+          <div>Number of Calls</div>
           <div></div>
         </div>
-        {/* <Button>Show History</Button> */}
+        <Button>Show History</Button>
       </div>
     </>
   );
